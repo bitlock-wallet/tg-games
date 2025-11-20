90fb33c30edc5c2a3fb0294f21ec48154f7c6cb5
Edin Ćehić <edinehi53@gmail.com>
2025-11-20 12:27:00 +0100

    refractor(lumberjack-game): removed the .next folder

--- .next/dev/static/chunks/src_app_actions_upsertUserAndScore_ts_37eeeb1d._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_acb653ee._.js"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/src_app_layout_tsx_193ee880._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__0f0ba101._.css",
-  "static/chunks/src_app_actions_upsertUserAndScore_ts_70e5f8bc._.js",
-  "static/chunks/_77e3175e._.js"
-],
-    source: "dynamic"
-});
